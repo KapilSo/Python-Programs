868103506133726f85ebd4f3855b029dac63ed02
Kapil Sonwane <115527503+KapilSo@users.noreply.github.com>
2025-12-19 15:34:37 +0530

    Add files via upload

---
+++ Chapter 3 Practice Set/problem2.py
@@ -0,0 +1,4 @@
+letter = '''Dear <|Name|>,
+You are selected!
+<|Date|>'''
+print(letter.replace("<|Name|>", "Kapil").replace("<|Date|>", "31/08/2025"))
---
+++ Chapter 3 Practice Set/problem3.py
@@ -0,0 +1,2 @@
+name = "Kapil is a good  boy."
+print(name.find("  "))
